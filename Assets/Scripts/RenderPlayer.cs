b106dbe07a20209500e3533298380b82a45950c3
ycbencheng <ycbencheng@gmail.com>
2022-02-07 13:17:08 -0700

    Update player placement on horizontal plane

---
+++ Assets/Scripts/RenderPlayer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+
+public class RenderPlayer : MonoBehaviour
+{
+    public GameObject placementIndicator;
+
+    ARRaycastManager arRaycastManager;
+    Pose placementPose;
+    bool placementPoseIsValid = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+
+        arRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+        }
+    }
+
+    void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+}
