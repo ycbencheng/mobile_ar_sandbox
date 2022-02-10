199f5ed04b10704312cb61c85a717c54df76aec1
ycbencheng <ycbencheng@gmail.com>
2022-02-10 08:00:52 -0700

    Reformat battle system

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -8,78 +8,66 @@ using System;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
-public class BattleSystem : MonoBehaviour
-{
-    public BattleState state;
-    public GameObject battle;
-    public TMP_Text dialogueText;
-    public Button resetButton;
+public class BattleSystem:MonoBehaviour {
+  public BattleState batleState;
+  public GameObject battle;
+  public TMP_Text dialogueText;
+  public Button resetButton;
 
-    Animator playerAnimator;
-    Animator enemyAnimator;
+  Animator playerAnimator;
+  Animator enemyAnimator;
 
-    Unit playerUnit;
-    Unit enemyUnit;
+  Unit playerUnit;
+  Unit enemyUnit;
 
-    bool isDead = false;
+  bool isDead = false;
 
-    void Start()
-    {
-        state = BattleState.START;
-        SetupBattle();
+  public void OnResetButton() {
+    SceneManager.LoadScene("MainScene");
+  }
+
+  void Start() {
+    batleState = BattleState.START;
+    SetupBattle();
+  }
+
+  void SetupBattle() {
+    GameObject playerGO = battle.transform.Find("DogKnight").gameObject;
+    playerUnit = playerGO.GetComponent<Unit>();
+    playerAnimator = playerGO.GetComponent<Animator>();
+
+    GameObject enemyGO = battle.transform.Find("Slime").gameObject;
+    enemyUnit = enemyGO.GetComponent<Unit>();
+    enemyAnimator = enemyGO.GetComponent<Animator>();
+
+    batleState = BattleState.PLAYERTURN;
+  }
+
+  void Update() {
+    if (batleState == BattleState.PLAYERTURN && Input.touchCount > 0) {
+      Touch touch = Input.GetTouch(0);
+
+      switch (touch.phase) {
+        case TouchPhase.Began:
+          playerAnimator.SetBool("isScreenTouch", true);
+          break;
+
+        case TouchPhase.Ended:
+          playerAnimator.SetBool("isScreenTouch", false);
+          enemyAnimator.SetBool("isScreenTouch", true);
+
+          isDead = enemyUnit.TakeDamage(playerUnit.damage);
+          break;
+      }
     }
 
-    void SetupBattle()
-    {
-        GameObject playerGO = battle.transform.Find("DogKnight").gameObject;
-        playerUnit = playerGO.GetComponent<Unit>();
-        playerAnimator = playerGO.GetComponent<Animator>();
-
-        GameObject enemyGO = battle.transform.Find("Slime").gameObject;
-        enemyUnit = enemyGO.GetComponent<Unit>();
-        enemyAnimator = enemyGO.GetComponent<Animator>();
-
-        state = BattleState.PLAYERTURN;
+    if (isDead) {
+     batleState = BattleState.WON;
+      EndBattle();
     }
+  }
 
-
-    void Update()
-    {       
-        if (state == BattleState.PLAYERTURN && Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            switch (touch.phase)
-            {
-                case TouchPhase.Began:
-                    playerAnimator.SetBool("isScreenTouch", true);
-                    break;
-
-                case TouchPhase.Ended:
-                    playerAnimator.SetBool("isScreenTouch", false);
-                    enemyAnimator.SetBool("isScreenTouch", true);
-
-                    isDead = enemyUnit.TakeDamage(playerUnit.damage);
-                    break;
-            }
-
-        }
-        Debug.Log(isDead);
-
-        if (isDead)
-        {
-            state = BattleState.WON;
-            EndBattle();
-        }
-    }
-
-    public void OnResetButton()
-    {
-        SceneManager.LoadScene("MainScene");
-    }
-
-    void EndBattle()
-    {
-        dialogueText.text = "You won the battle!";
-    }
+  void EndBattle() {
+     dialogueText.text = "You won the battle!";
+  }
 }
