29592d4fae6b27856c889524b62551642401b722
ycbencheng <ycbencheng@gmail.com>
2022-02-10 16:58:19 -0700

    Match voice with attack

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -1,75 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using TMPro;
-using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+// using UnityEngine.UI;
+// using TMPro;
+// using System;
 
-public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+// public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
-public class BattleSystem:MonoBehaviour {
-  public BattleState batleState;
-  public GameObject battle;
-  public TMP_Text dialogueText;
-  public Button resetButton;
+// public class BattleSystem:MonoBehaviour {
+//   public BattleState battleState;
+//   public GameObject battle;
+//   public TMP_Text dialogueText;
+//   public Button resetButton;
 
-  Animator playerAnimator;
-  Animator enemyAnimator;
+//   Animator playerAnimator;
+//   Animator enemyAnimator;
 
-  Unit playerUnit;
-  Unit enemyUnit;
+//   Unit playerUnit;
+//   Unit enemyUnit;
 
-  bool isDead = false;
+//   Random rnd = new Random();
 
-  public void OnResetButton() {
-    SceneManager.LoadScene("MainScene");
-  }
+//   bool isDead = false;
 
-  void Start() {
-    batleState = BattleState.START;
-    resetButton.gameObject.SetActive(false);
-    SetupBattle();
-  }
+//   public void OnResetButton() {
+//     SceneManager.LoadScene("MainScene");
+//   }
 
-  void SetupBattle() {
-    GameObject playerGO = battle.transform.Find("DogKnight").gameObject;
-    playerUnit = playerGO.GetComponent<Unit>();
-    playerAnimator = playerGO.GetComponent<Animator>();
+//   void Start() {
+//     battleState = BattleState.START;
+//     resetButton.gameObject.SetActive(false);
+//     SetupBattle();
+//   }
 
-    GameObject enemyGO = battle.transform.Find("Slime").gameObject;
-    enemyUnit = enemyGO.GetComponent<Unit>();
-    enemyAnimator = enemyGO.GetComponent<Animator>();
+//   void SetupBattle() {
+//     GameObject playerGO = battle.transform.Find("DogKnight").gameObject;
+//     playerUnit = playerGO.GetComponent<Unit>();
+//     playerAnimator = playerGO.GetComponent<Animator>();
 
-    batleState = BattleState.PLAYERTURN;
-  }
+//     GameObject enemyGO = battle.transform.Find("Slime").gameObject;
+//     enemyUnit = enemyGO.GetComponent<Unit>();
+//     enemyAnimator = enemyGO.GetComponent<Animator>();
 
-  void Update() {
-    if (batleState == BattleState.PLAYERTURN && Input.touchCount > 0) {
-      Touch touch = Input.GetTouch(0);
+//     battleState = BattleState.PLAYERTURN;
+//   }
 
-      switch (touch.phase) {
-        case TouchPhase.Began:
-          playerAnimator.SetBool("isScreenTouch", true);
-          break;
+//   void Update() {
+//     if (battleState == BattleState.PLAYERTURN && Input.touchCount > 0) {
+//       Touch touch = Input.GetTouch(0);
 
-        case TouchPhase.Ended:
-          playerAnimator.SetBool("isScreenTouch", false);
-          enemyAnimator.SetBool("isScreenTouch", true);
+//       switch (touch.phase) {
+//         case TouchPhase.Began:
+//           playerAnimator.SetBool("isScreenTouch", true);
+//           break;
 
-          isDead = enemyUnit.TakeDamage(playerUnit.damage);
-          break;
-      }
-    }
+//         case TouchPhase.Ended:
+//           playerAnimator.SetBool("isScreenTouch", false);
+//           enemyAnimator.SetBool("isScreenTouch", true);
 
-    if (isDead) {
-      batleState = BattleState.WON;
-      EndBattle();
-    }
-  }
+//           isDead = enemyUnit.TakeDamage(playerUnit.damage);
+//           break;
+//       }
+//     }
 
-  void EndBattle() {
-     dialogueText.text = "You won the battle!";
-     resetButton.gameObject.SetActive(true);
-  }
-}
+//     if (isDead) {
+//       battleState = BattleState.WON;
+//       EndBattle();
+//     }
+//   }
+
+//   void EndBattle() {
+//      dialogueText.text = "You won the battle!";
+//      resetButton.gameObject.SetActive(true);
+//   }
+// }
---
+++ Assets/Scripts/BattleVoiceSystem.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using KKSpeech;
+
+using Random = System.Random;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleVoiceSystem:MonoBehaviour {
+  public BattleState battleState;
+  public GameObject battle;
+  public TMP_Text dialogueText;
+  public Button resetButton;
+  public Button recordAttackButton;
+
+  Animator playerAnimator;
+  Animator enemyAnimator;
+
+  Unit playerUnit;
+  Unit enemyUnit;
+
+  Random rnd = new Random();
+
+  bool isDead = false;
+  string[] wordsArray = {"Hello", "Welcome"};
+  string spell = "";
+
+  public void OnResetButton() {
+    SceneManager.LoadScene("MainScene");
+  }
+
+  void Start() {
+    battleState = BattleState.START;
+    SetupVoice();
+    SetupBattle();
+  }
+
+  void SetupVoice() {
+    if (SpeechRecognizer.ExistsOnDevice()) {
+      SpeechRecognizerListener listener = GameObject.FindObjectOfType<SpeechRecognizerListener>();
+      listener.onAuthorizationStatusFetched.AddListener(OnAuthorizationStatusFetched);
+      listener.onAvailabilityChanged.AddListener(OnAvailabilityChange);
+      listener.onErrorDuringRecording.AddListener(OnError);
+      listener.onErrorOnStartRecording.AddListener(OnError);
+      listener.onFinalResults.AddListener(OnFinalResult);
+      listener.onEndOfSpeech.AddListener(OnEndOfSpeech);
+      SpeechRecognizer.RequestAccess();
+    } else {
+      dialogueText.text = "This device doesn't support speech recognition";
+      recordAttackButton.gameObject.SetActive(false);
+    }
+  }
+
+  void SetupBattle() {
+    GameObject playerGO = battle.transform.Find("DogKnight").gameObject;
+    playerUnit = playerGO.GetComponent<Unit>();
+    playerAnimator = playerGO.GetComponent<Animator>();
+
+    GameObject enemyGO = battle.transform.Find("Slime").gameObject;
+    enemyUnit = enemyGO.GetComponent<Unit>();
+    enemyAnimator = enemyGO.GetComponent<Animator>();
+
+    int index = rnd.Next(wordsArray.Length);
+    spell = wordsArray[index];
+
+    battleState = BattleState.PLAYERTURN;
+
+    dialogueText.text = $"Say {spell}";
+  }
+
+  void EndBattle() {
+    dialogueText.text = "You won the battle!";
+  }
+
+  public void OnFinalResult(string result) {
+    StartCoroutine(BattleProcess(result));
+  }
+
+  IEnumerator BattleProcess (string result) {
+    if (battleState == BattleState.PLAYERTURN && result == spell) {
+      playerAnimator.SetBool("isScreenTouch", true);
+
+      dialogueText.text = "Correct Spell!";
+
+      yield return new WaitForSeconds(1f);
+
+      playerAnimator.SetBool("isScreenTouch", false);
+
+      isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+    } else {
+      dialogueText.text = $"Say {spell} again.";
+
+      recordAttackButton.GetComponentInChildren<Text>().text = "Try again!";
+      recordAttackButton.gameObject.SetActive(true);
+    }
+
+    if (isDead) {
+      enemyAnimator.SetBool("isScreenTouch", true);
+      battleState = BattleState.WON;
+      EndBattle();
+    }
+  }
+
+  public void OnPartialResult(string result) {
+    dialogueText.text = $"Say {spell} again.";
+  }
+
+  public void OnAvailabilityChange(bool available) {
+    recordAttackButton.gameObject.SetActive(available);
+
+    if (!available) {
+      dialogueText.text = "Speech Recognition not available";
+    } else {
+      dialogueText.text = "Say something";
+    }
+  }
+
+  public void OnAuthorizationStatusFetched(AuthorizationStatus status) {
+    switch (status) {
+      case AuthorizationStatus.Authorized:
+        recordAttackButton.gameObject.SetActive(true);
+        break;
+      default:
+        recordAttackButton.gameObject.SetActive(false);
+        dialogueText.text = "Cannot use Speech Recognition, authorization status is " + status;
+        break;
+    }
+  }
+
+  public void OnEndOfSpeech() {
+    recordAttackButton.GetComponentInChildren<Text>().text = "Start Recording";
+  }
+
+  public void OnError(string error) {
+    recordAttackButton.GetComponentInChildren<Text>().text = "Error. Try again!";
+    recordAttackButton.gameObject.SetActive(true);
+  }
+
+  public void OnStartRecordingPressed() {
+    if (SpeechRecognizer.IsRecording()) {
+      #if ((UNITY_IOS && !UNITY_EDITOR) || (UNITY_ANDROID && !UNITY_EDITOR))
+        SpeechRecognizer.StopIfRecording();
+        recordAttackButton.GetComponentInChildren<Text>().text = "Start Recording";
+        recordAttackButton.gameObject.SetActive(false);
+      #endif
+    } else {
+      SpeechRecognizer.StartRecording(true);
+      recordAttackButton.GetComponentInChildren<Text>().text = "Stop Recording";
+    }
+  }
+}
